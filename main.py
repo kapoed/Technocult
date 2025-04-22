6d443cdfeee2f2764cdf569d38634814002edb55
kapoed <ilya.karto@gmail.com>
2025-04-22 22:35:35 +0300

    create project

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-print(123)
+import secrets
+from flask import Flask, render_template, redirect
+from forms.register import LoginForm
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = secrets.token_hex(16)
+
+
+@app.route('/')
+def index():
+    return render_template('base.html')
+
+
+@app.route('/register', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('register.html', title='Авторизация', form=form)
+
+
+if __name__ == '__main__':
+    app.run(port=8080, host='127.0.0.1', debug=True)
